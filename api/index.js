1d8aaf8da0682ae9bf1894d78de65fb756279bdf
Định Lê Trí <letridinh2002@gmail.com>
2022-08-24 21:35:53 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import roomsRoute from "./routes/rooms.js";
+import hotelsRoute from "./routes/hotels.js";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connect to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+//middlewares
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((req, res, next) => {
+  console.log("Hi, I'm middleware!");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connect to backend");
+});
